0eec5d0fcec6deb19e248dae7acd29ca85d23cb9
alexfac <alexandre.sarrazin@ens.uvsq.fr>
2020-03-17 14:12:11 +0100

    First commit

---
+++ pglp_4.2/src/test/java/uvsq21603110/AppTest.java
@@ -0,0 +1,17 @@
+package uvsq21603110;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    
+}
